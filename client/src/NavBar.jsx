9b26994e0e4d78356498caa5f7ff62057938ff56
Sota Giorgadze <giorgadze@inbox.lv>
2023-04-10 17:38:54 +0300

    prefetch test

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -7,7 +7,12 @@ const NavBar = () => {
   return (
     <div className="bg-purple-300 h-14 flex items-center gap-2">
       <p className="text-sm p-2 ml-2 text-teal-950">Sveiki, {loggedInUser}</p>
-      <button to="/iesniegt" className="border border-white px-2 py-1 rounded-md text-white hover:bg-purple-500 transition-all" onClick={logout}>
+      <button
+        to="/iesniegt"
+        className="border border-white px-2 py-1 rounded-md text-white hover:bg-purple-500 transition-all"
+        onClick={logout}
+        prefetch={false}
+      >
         Izrakstīties
       </button>
     </div>
