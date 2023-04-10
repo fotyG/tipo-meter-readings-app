0955d2318cebb73805de9964bd8e6c1bd4fc8566
Sota Giorgadze <giorgadze@inbox.lv>
2023-04-10 20:10:42 +0300

    Init local

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import AppRoutes from "./AppRoutes"
 import { UserContextProvider } from "./UserContext"
 
 function App() {
-  axios.defaults.baseURL = "https://api-tipo-app.vercel.app/api/v1/"
+  axios.defaults.baseURL = "http://localhost:6969/api/v1/"
   axios.defaults.withCredentials = true;
 
   return (
