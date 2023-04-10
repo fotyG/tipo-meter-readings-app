47981661b85f85bbe671852fc1080978d9bbe3ec
Sota Giorgadze <giorgadze@inbox.lv>
2023-04-10 13:21:12 +0300

    test1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import AppRoutes from "./AppRoutes"
 import { UserContextProvider } from "./UserContext"
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:6969/api/v1/"
+  axios.defaults.baseURL = "https://tipo-app-api.vercel.app/api/v1/"
   axios.defaults.withCredentials = true;
 
   return (
