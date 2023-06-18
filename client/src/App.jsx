24bd26476daf3e421b88c1730c8057a02d1b0d84
Sota Giorgadze <giorgadze@inbox.lv>
2023-06-18 20:15:16 +0300

    * fix base-url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@ import axios from "axios";
 import AppRoutes from "./AppRoutes";
 import { UserContextProvider } from "./UserContext";
 
+// "http://localhost:6969/api/v2"
 function App() {
-  axios.defaults.baseURL =
-    "http://localhost:6969/api/v2" || "https://api-tipo-app.vercel.app/api/v2/";
+  axios.defaults.baseURL = "https://api-tipo-app.vercel.app/api/v2/";
 
   return (
     <UserContextProvider>
